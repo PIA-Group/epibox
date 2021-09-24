0edb4e3f2c4cb7c8153385cd9cd0ed78f98fd14b
Ana <anascacais@gmail.com>
2021-09-24 11:55:09 +0100

    plot multiple acquisitions

--- epibox/mqtt_performance/test.py
+++ epibox/mqtt_performance/test.py
@@ -7,47 +7,58 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 directory = '/home/ana/Documents/epibox'
-startup_file = 'mqtt_timestamps_startup_2021-09-22 11-55-27.txt'
-time = startup_file.split('_')[-1]
-run_file = 'mqtt_timestamps_startup_' + time
 
-file_path = os.path.join(directory, startup_file)
-df = pd.read_csv(file_path, sep=', ', header=None, names=['timestamp', 'id', 'key'])
+full_df = pd.DataFrame(columns=['timestamp', 'time', 'key', 'file'])
+labels_timestamps = []
 
-file_path = os.path.join(directory, run_file)
-df_aux = pd.read_csv(file_path, sep=', ', header=None, names=['timestamp', 'id', 'key'])
-df = df.append(df_aux, ignore_index=True)
+for startup_file in [f for f in os.listdir(directory) if 'startup' in f]:
 
-ids = df['id'].unique()
+    startup_time = startup_file.split('_')[-1]
+    run_file = 'mqtt_timestamps_run_' + startup_time
 
-times = pd.DataFrame([], columns=['timestamp', 'time', 'key'])
+    file_path = os.path.join(directory, startup_file)
+    df = pd.read_csv(file_path, sep=', ', header=None, names=['timestamp', 'id', 'key'])
 
-for id in ids:
-    a = df.loc[df['id'] == id]
-    try:
-        result = a.iloc[1]['timestamp'] - a.iloc[0]['timestamp']
-        print(result)
+    file_path = os.path.join(directory, run_file)
+    df_aux = pd.read_csv(file_path, sep=', ', header=None, names=['timestamp', 'id', 'key'])
+    df = df.append(df_aux, ignore_index=True)
 
-    except Exception as e:
-        print('{} -- {}'.format(e, a))
+    ids = df['id'].unique()
 
-    times = times.append(pd.DataFrame([[a.iloc[0,0], result, a.iloc[0,2]]], columns=['timestamp', 'time', 'key']), ignore_index=True)
-    print(times.iloc[0]['timestamp'])
-    #times['timestamp'] = times['timestamp'] - times.iloc[0]['timestamp']
-    print(times)
+    times = pd.DataFrame([], columns=['timestamp', 'time', 'key'])
 
-times['timestamp'] = times['timestamp'] - times.iloc[0]['timestamp']
-data_group = times.loc[times['key'] == 'DATA']
+    for id in ids:
+        a = df.loc[df['id'] == id]
+        try:
+            result = a.iloc[1]['timestamp'] - a.iloc[0]['timestamp']
+            print(result)
 
-max_latency = times['time'].max()
+        except Exception as e:
+            print('{} -- {}'.format(e, a))
 
+        times = times.append(pd.DataFrame([[a.iloc[0,0], result, a.iloc[0,2]]], columns=['timestamp', 'time', 'key']), ignore_index=True)
+        print(times)
+
+    times['timestamp'] = times['timestamp'] - times.iloc[0]['timestamp']
+    times['file'] = startup_time
+
+    full_df = full_df.append(times, ignore_index=True)
+
+    data_group = times.loc[times['key'] == 'DATA']
+    labels_timestamps += [[data_group.iloc[0]['timestamp'], data_group.iloc[-1]['timestamp']]]
+    
+    
+max_latency = full_df['time'].max()
     
 plt.figure()
-sns.lineplot(data=times, x='timestamp', y='time')
-plt.axvline(data_group.iloc[0]['timestamp'], color='tab:orange')
-plt.text(data_group.iloc[0]['timestamp'], max_latency, 'START', color='tab:orange')
-plt.axvline(data_group.iloc[-1]['timestamp'], color='tab:orange')
-plt.text(data_group.iloc[-1]['timestamp'], max_latency, 'END', color='tab:orange')
+sns.lineplot(data=full_df, x='timestamp', y='time', hue='file', alpha=0.5)
+
+for label in labels_timestamps:
+    plt.axvline(label[0], color='--k')
+    plt.text(label[0], max_latency, 'START', color='--k')
+    plt.axvline(label[-1], color='tab:orange')
+    plt.text(label[-1], max_latency, 'END', color='tab:orange')
+
 plt.xlabel('time since start [s]')
 plt.ylabel('latency [s]')
 plt.show()
