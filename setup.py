cdbb48e756ada9d3f0b892d6ff525cb7c658f8c0
anascacais <anascacais@gmail.com>
2024-08-04 16:23:06 +0100

    updated version

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/anascacais/epibox",
-    version="2.0.5",
+    version="3.0.0",
     zip_safe=False,
 )
